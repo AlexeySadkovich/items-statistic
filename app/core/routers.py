65d8ff9613a532248edcd96ea39fcfb5b5a11b76
Alexey Sadkovich <sadkovich01@mail.ru>
2020-12-09 10:18:38 +0300

    Update response structure

--- app/core/routers.py
+++ app/core/routers.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from fastapi import APIRouter
 
 from services.database import crud
-from services.database.crud import update_queries_stat
 from services.database.schemas import QueryCreate
 
 router = APIRouter()
@@ -16,7 +15,7 @@ async def add_query(query: QueryCreate):
     :return: id of saved query
     """
     saved_query = await crud.save_query(query)
-    return str(saved_query.id)
+    return {"id": str(saved_query.id)}
 
 
 @router.get("/stat")
@@ -24,16 +23,22 @@ async def get_statistic(query_id: str,
                         time_from: datetime = None,
                         time_to: datetime = None):
     """
-    View for pulling statistic of the query specified by id
+    View for pulling statistic about amount of items
+    for query specified by id
     :return: list of timestamps
     """
     result = await crud.get_statistic_for_query(query_id, "count", time_from, time_to)
-    return result
+    return {"id": query_id, "timestamps": result}
 
 
 @router.get("/stat/top")
 async def get_top_items(query_id: str,
                         time_from: datetime = None,
                         time_to: datetime = None):
+    """
+    View for pulling statistic about top items for
+    query specified by id
+    :return:
+    """
     result = await crud.get_statistic_for_query(query_id, "top_items", time_from, time_to)
-    return result
+    return {"id": query_id, "timestamps": result}
